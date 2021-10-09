0ac0dfaab1292a14ff8d046005cce3c3bb009306
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2021-10-09 12:28:25 -0300

    Add home component
    Add home component;
    Add rounting of the context;
    Add module of the context;

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from "@angular/core";
+
+import { HomeComponent } from "./page/home/home.component";
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+  ],
+  imports: [],
+  providers: []
+})
+export class HomeModule { }
