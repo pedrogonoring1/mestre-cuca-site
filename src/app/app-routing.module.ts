bde103a5fe3b8e99d8b180c4ad46c26cc4c92524
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2021-10-10 21:10:32 -0300

    Add routing to page not found

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule } from "@angular/core";
+import { Component, NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AutenticationRoutingModule } from "./autenticacao/autentication-routing.module";
 import { HomeRoutingModule } from "./home/home-routing.module";
 import { RecipeModule } from "./recipes/recipe.module";
+import { NotFoundComponent } from "./shared/not-found/not-found.component";
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'not-found', component: NotFoundComponent }
 ];
 
 @NgModule({
