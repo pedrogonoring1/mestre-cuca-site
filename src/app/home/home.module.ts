1f38eea9a081996b66358d5101bc10898a5e607f
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2021-10-10 21:12:23 -0300

    Add feature in home component

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,26 @@
-import { NgModule } from "@angular/core";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser'
 
 import { HomeComponent } from "./page/home/home.component";
+import { CarouselHomeComponent } from './components/carousel-home/carousel-home.component';
+import { CarouselCategoriasComponent } from './components/carousel-categorias/carousel-categorias.component';
+import { CardHotRecipesComponent } from './components/card-hot-recipes/card-hot-recipes.component';
+
+import { IvyCarouselModule } from 'angular-responsive-carousel';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
 
 @NgModule({
   declarations: [
     HomeComponent,
+    CarouselHomeComponent,
+    CarouselCategoriasComponent,
+    CardHotRecipesComponent,
   ],
-  imports: [],
-  providers: []
+  imports: [BrowserModule, IvyCarouselModule, TooltipModule.forRoot()],
+  providers: [],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
 })
 export class HomeModule { }
