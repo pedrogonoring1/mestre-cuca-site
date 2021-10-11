f21278a0f2cdf25d90ed0554f4fdc1aa1f80dc61
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2021-10-10 21:11:32 -0300

    Adjust app component html and module to incluid shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { AutenticationModule } from './autenticacao/autentication.module';
 import { HomeModule } from './home/home.module';
 import { RecipeRoutingModule } from './recipes/recipe-routing.module';
+import { FooterPageComponent } from './shared/footer-page/footer-page.component';
+import { HeaderPageComponent } from './shared/header-page/header-page.component';
+import { NotFoundComponent } from './shared/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterPageComponent,
+    HeaderPageComponent,
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     AutenticationModule,
     HomeModule,
-    RecipeRoutingModule
+    RecipeRoutingModule,
+    TooltipModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
 })
 export class AppModule { }
