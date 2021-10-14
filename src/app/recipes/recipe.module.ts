c51768f95c9bb6116c62edf7841de90da588ab10
Pedro Gonoring <53987520+pedrogonoring1@users.noreply.github.com>
2021-10-14 00:17:17 -0300

    Adjust recipe module

--- src/app/recipes/recipe.module.ts
+++ src/app/recipes/recipe.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from "@angular/core";
 import { AddRecipesComponent } from "./page/add/add-recipes/add-recipes.component";
 import { ListingRecipesComponent } from "./page/listing/listing-recipes/listing-recipes.component";
 
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { BrowserModule } from "@angular/platform-browser";
+
 @NgModule ({
   declarations: [
     AddRecipesComponent,
     ListingRecipesComponent,
   ],
-  imports: [],
+  imports: [BrowserModule, TooltipModule.forRoot()],
   providers: []
 })
 export class RecipeModule { }
